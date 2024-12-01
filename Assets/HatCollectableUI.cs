55ca79ad6181e1c0bd02ac6f934320e4245ccb03
plhacko <plhacko@gmail.com>
2024-12-01 11:59:00 +0100

    hat UI fix

--- Assets/HatCollectableUI.cs
+++ Assets/HatCollectableUI.cs
@@ -4,6 +4,8 @@ public class HatCollectableUI : MonoBehaviour
 {
     void Start()
     {
+        gameObject.SetActive(false);
+
         GlobalManager.Instance.OnHatPickedUp += () => { gameObject.SetActive(true); };
         GlobalManager.Instance.OnHatLost += () => { gameObject.SetActive(false); };
     }
