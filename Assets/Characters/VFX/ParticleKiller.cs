1271bc1ff16d723bc361c40ded4f63f10b76d3e3
Ondřej Čermák <cermaak.ondrej@gmail.com>
2024-12-01 15:59:26 +0100

    VFX Update

--- Assets/Characters/VFX/ParticleKiller.cs
+++ Assets/Characters/VFX/ParticleKiller.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class ParticleKiller : MonoBehaviour
 {
+    [SerializeField] private float deathTimeMultiplier = 1f;
     [SerializeField] private ParticleSystem particleSystem;
 
     private float deathTime;
@@ -10,6 +11,7 @@ public class ParticleKiller : MonoBehaviour
     {
         // Get the total duration of the particle system
         deathTime = particleSystem.main.duration + particleSystem.main.startLifetime.constant;
+        deathTime *= deathTimeMultiplier;
     }
 
     private void Update()
