c84d7f51ab4ab4b19b2ea0403ebd0b3ad047bfbc
Tomas Bubenicek <tom@tombuben.eu>
2024-12-01 13:33:55 +0100

    The scene end now looks better in 2d

--- Assets/EnableIn3D.cs
+++ Assets/EnableIn3D.cs
@@ -2,15 +2,17 @@ using UnityEngine;
 
 public class EnableIn3D : MonoBehaviour
 {
+    public bool disableIn3D;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        gameObject.SetActive(GlobalManager.Instance.is3D);
+        OnTogglePerspective(GlobalManager.Instance.is3D);
         GlobalManager.Instance.OnTogglePerspective += OnTogglePerspective; 
     }
 
     private void OnTogglePerspective(bool is3D)
     {
-        gameObject.SetActive(is3D);
+        gameObject.SetActive(disableIn3D ? !is3D : is3D);
     }
 }
